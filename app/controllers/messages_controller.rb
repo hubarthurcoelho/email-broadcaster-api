4654a293264ab115c5a2f15ece18d1641e4b8c8c
Arthur Coelho <arthurcoelho7885@gmail.com>
2024-08-14 14:48:52 -0300

    feat: simple message creation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,32 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all
+    render json: @messages
   end
+
+  def show
+    @message = Message.includes(:message_recipients).find(params[:id])
+    render json: @message.as_json(only: [ :body, :title ], include: { message_recipients: { only: [ :email ] } })
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      render json: @message, status: :created
+
+      if params[:emails].present?
+        params[:emails].each do |email|
+          @message.message_recipients.create(email: email, sent_at: Time.now)
+        end
+      end
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:body, :title)
+    end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ApplicationRecord
+  has_many :message_recipients
+
+  validates :title, presence: false, length: { maximum: 50 }
+  validates :body, presence: true, length: { maximum: 500 }
 end
