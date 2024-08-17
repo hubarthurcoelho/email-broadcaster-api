4eb6a448e18f90d21ab6a325f0e50581d1740cb3
Arthur Coelho <99970472+hubarthurcoelho@users.noreply.github.com>
2024-08-17 19:49:18 -0300

    feat: created email validator service (#6)

---
+++ app/services/email_validator_service.rb
@@ -0,0 +1,22 @@
+class EmailValidatorService
+  def validate_multiple(emails)
+    errors = []
+
+    emails.each do |email|
+      unless validate(email).nil?
+        errors << email
+      end
+    end
+
+    errors
+  end
+
+  def validate(email)
+    # in this class we could use an API to check the validity of an email address in the future
+    unless email.is_a?(String) && email =~ URI::MailTo::EMAIL_REGEXP
+      return "#{email} is not a valid email address"
+    end
+
+    nil
+  end
+end
